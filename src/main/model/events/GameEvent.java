687b7bd7272deec14d48ae930dbd02fa7bc3f2a1
amyjzhu <ajyzhu@gmail.com>
2017-12-07 21:48:29 -0800

    Tweaks to load

--- src/main/model/events/GameEvent.java
+++ src/main/model/events/GameEvent.java
@@ -14,26 +14,31 @@ public class GameEvent {
 
     @FunctionalInterface
     public interface Animation {
-        public void run();
+        public void run(Object[] args);
     }
 
     Map<String, Animation> eventsMap;
 
     public GameEvent() {
         eventsMap = new HashMap<>();
-        eventsMap.put("update",() -> Application.getGameData().newValue(Integer.toString(Day.getDay())));
-        eventsMap.put("updateToText", () -> {Application.getGameData().newValue("Hey!"); Application.update();}); // not sure how to add params at this moment... maybe check a params thing? hm
-        eventsMap.put("replaceText", () -> Application.update());
-        eventsMap.put("crazyNumber", () -> {
+        eventsMap.put("update",(args) -> Application.getGameData().newValue(Integer.toString(Day.getDay())));
+        eventsMap.put("updateToText", (args) -> {Application.getGameData().newValue("Hey!"); Application.update();}); // not sure how to add params at this moment... maybe check a params thing? hm
+        eventsMap.put("replaceText", (args) -> Application.update());
+        eventsMap.put("crazyNumber", (args) -> {
             int rand = new Random().nextInt();
             Application.getGameData().newValue(Integer.toString(rand));
          Application.update();});
     }
 
     public void run(String animationTitle) {
+        // if arguments is null
+        run(animationTitle, null);
+    }
+
+    public void run(String animationTitle, Object[] arguments) {
         Animation animation = eventsMap.get(animationTitle.trim());
         if (animation != null) {
-            animation.run();
+            animation.run(arguments);
         } // should throw an exception... but okay
         System.out.println(animationTitle);
     }

32a238719bf4b38cfc29ac7e099d0c8ec461b462
amyjzhu <ajyzhu@gmail.com>
2017-12-17 17:58:54 -0800

    Playing with Reflection example

---
+++ src/main/data/Reflection.java
@@ -0,0 +1,35 @@
+package main.data;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by gijin on 2017-12-17.
+ */
+public class Reflection {
+
+
+    static Map<String, Class[]> eventsToArguments;
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        // take in the name
+        Class[] params = new Class[]{Integer.TYPE, Integer.TYPE};
+        eventsToArguments = new HashMap<>();
+        eventsToArguments.put("add", params);
+
+        Class theClass = Class.forName("main.data.Reflection");
+        Object[] argus = new Object[]{2, 3};
+        Method m = theClass.getMethod("add", params);
+        Object inst = theClass.newInstance();
+        Object ret = m.invoke(inst, argus);
+        System.out.println(ret.toString());
+
+    }
+
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
